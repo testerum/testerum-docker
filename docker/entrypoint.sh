68e5dc8948bfb466d03b74e7aafef4823a97dc32
Cristian Mocanu <cvmocanu@gmail.com>
2020-07-06 18:32:59 +0200

    entrypoint.sh: give error if "/tests" is not mounted

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,13 @@
 
 TESTERUM_DIR=/opt/testerum_home/testerum
 
+if [ ! -d "/tests" ]; then
+    echo "ERROR: please mount your tests directory at /tests"
+    exit 1
+fi
+
+echo "Starting Testerum..."
+
 exec "${TESTERUM_DIR}/runner/bin/testerum-runner.sh" \
     --repository-directory=/tests \
     --managed-reports-directory=/reports \

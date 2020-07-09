7f50267990dcc473e279e7616ebc2ada5d89ec81
Cristian Mocanu <cvmocanu@gmail.com>
2020-07-09 19:29:35 +0200

    build.sh: also tag as "latest"

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 export version=3.1.19
-docker build --tag testerum:$version --build-arg testerum_version=$version .
+docker build \
+    --tag testerum:$version \
+    --tag testerum:latest \
+    --build-arg testerum_version=$version \
+    .

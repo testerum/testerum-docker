f3206b3b23c4f857f1dc0bfbdcdedb176c2025d5
Cristian Mocanu <cvmocanu@gmail.com>
2020-07-09 19:21:18 +0200

    build.sh: use latest released version (+ tag the docker image with the same version

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build --tag testerum:1.2 --build-arg testerum_version=3.1.4 .
+#!/usr/bin/env bash
+
+export version=3.1.19
+docker build --tag testerum:$version --build-arg testerum_version=$version .
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 docker run --rm -v /home/cvmocanu/temp/tests:/tests -v /home/cvmocanu/temp/reports:/reports --name testerum testerum:1.2
 

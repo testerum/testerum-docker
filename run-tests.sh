40edc8e37896c3af3b8b07e0cb3864fc595a0f48
Cristian Mocanu <cvmocanu@gmail.com>
2021-01-10 18:48:18 +0100

    release Testerum 5.0.2

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+script_dir="$(dirname "${0}")"
+source "${script_dir}/common.sh"
+
 export MSYS_NO_PATHCONV=1
 
-docker run -p8000:8000 --rm -v /c/temp/tutorial/tests:/tmp/project/tests -v /c/temp/tutorial/reports:/tmp/project/reports --name testerum testerum:1.2 --setting BASE_URL=sadfdsf --repository-directory /tmp/project/tests --managed-reports-directory /tmp/project/reports -e JAVA_OPTS=agent debug stuff
+docker run \
+    -p8000:8000 \
+    --rm \
+    -v /c/temp/tutorial/tests:/tmp/project/tests \
+    -v /c/temp/tutorial/reports:/tmp/project/reports \
+    --name testerum \
+    "testerum:${testerum_version}" \
+    --setting BASE_URL=sadfdsf \
+    --managed-reports-directory /tmp/project/reports \
+    -e JAVA_OPTS=agent debug stuff
 
 # docker run --rm -v /c/temp/tutorial/tests:/project/tests -v /c/temp/tutorial/reports:/project/reports --name testerum testerum:1.2 --output=CONSOLE

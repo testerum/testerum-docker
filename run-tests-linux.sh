8d5ed2611c22368d0e76cc0d59263befdca1840a
Cristian Mocanu <cvmocanu@gmail.com>
2021-06-09 20:42:40 +0200

    fix run-tests-linux.sh to run tests as the current user

--- run-tests-linux.sh
+++ run-tests-linux.sh
@@ -5,7 +5,8 @@ source "${script_dir}/common.sh"
 
 docker run \
     --rm \
-    -v /home/cvmocanu/temp/testerum/tests/testerum-tests:/tests \
-    -v /home/cvmocanu/temp/testerum/reports:/reports \
+    --volume /home/cvmocanu/temp/testerum/tests/testerum-tests:/tests \
+    --volume /home/cvmocanu/temp/testerum/reports:/reports \
     --name testerum \
+    --user "$(id -u "${USER}"):$(id -g "${USER}")" \
     "testerum/testerum:${testerum_version}"
